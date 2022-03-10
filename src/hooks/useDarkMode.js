edf5133a30ec87d628ae48f68cd43f50e71c031b
b1ggl3s <98510771+b1ggl3s@users.noreply.github.com>
2022-03-10 17:59:33 +0800

    Add files via upload

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+
+
+export default () => {
+
+  const [isDark, setIsDark] = useState(false)
+
+
+  useEffect(() => {
+
+    const className = "dark"
+
+    if (isDark) {
+
+      window.document.body.classList.add(className)
+
+    } else {
+
+      window.document.body.classList.remove(className)
+
+    }
+
+  }, [isDark])
+
+
+  return [isDark, setIsDark]
+
+}
